08c11b3af311464b17de618a4dd2b8a838185ba1
Dima Snisarenko <snisarenkodima@gmail.com>
2018-11-23 11:08:10 +0100

    Generate PDFs with Puppeteer

--- src/build.js
+++ src/build.js
@@ -1,8 +1,8 @@
 const handlebars = require('handlebars');
 const fs = require('fs-extra');
-const buildPdf = require('./utils/pdf');
 const markdownHelper = require('./utils/helpers/markdown');
 const templateData = require('./metadata/metadata');
+const Puppeteer = require('puppeteer');
 
 const srcDir = __dirname;
 const outputDir = __dirname + '/../dest';
@@ -19,6 +19,26 @@ const source = fs.readFileSync(srcDir + '/templates/index.html', 'utf-8');
 const template = handlebars.compile(source);
 const html = template(templateData);
 fs.writeFileSync(outputDir + '/index.html', html);
+
+buildPdf = async function(inputFile, outputFile) {
+  const browser = await Puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(`file://${inputFile}`, {
+    waitUntil: 'networkidle0'
+  });
+  await page.pdf({
+    path: outputFile,
+    format: 'A4',
+    border: 0,
+    margin: {
+      top: '2.54cm',
+      right: '2.54cm',
+      bottom: '2.54cm',
+      left: '2.54cm',
+    },
+  });
+  await browser.close();
+};
 
 // Build PDF
 buildPdf(outputDir + '/index.html', outputDir + '/william-shakespeare.national-poet.pdf');
