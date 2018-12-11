de7008e7f891520f4ca2039d0fbaea49282128db
Dima Snisarenko <snisarenkodima@gmail.com>
2018-12-11 19:38:43 +0100

    Keep base URL in metadata.js

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,6 +1,7 @@
 module.exports = {
   updated: new Date(),
   pdfFileName: 'william-shakespeare.national-poet.pdf',
+  baseUrl: 'https://github.com/sneas/cv-project',
   facts: {
     'Residence': 'London',
     'Homepage': '<a href="https://en.wikipedia.org/wiki/William_Shakespeare">https://en.wikipedia.org/wiki/William_Shakespeare</a>',
